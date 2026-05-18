9373cfefe2bac6f169cf8d3a7542fc3f19557e5b
Your Name <your@email.com>
2026-05-18 12:03:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_evotting
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_evotting/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_evotting
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
